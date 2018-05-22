9faf059f9b7343b119e9cf9158f80b0138d32f12
shixianqin <1658852581@qq.com>
2018-05-23 03:27:54 +0800

    Create calculate.js

---
+++ calculate.js
@@ -0,0 +1,48 @@
+/*
+    2018-5-23
+    https://github.com/shixianqin/calculate
+*/
+
+var Calculate = (function() {
+
+    function translate(a, b) {
+        var dot = /\./,
+            empty = "",
+            dotA = a.toString().split(dot)[1] || empty,
+            dotB = b.toString().split(dot)[1] || empty,
+            max = Math.max(dotA.length, dotB.length);
+
+        return {
+            a: Number(a.toFixed(max).replace(dot, empty)),
+            b: Number(b.toFixed(max).replace(dot, empty)),
+            max: max
+        }
+    }
+
+    return {
+
+        add: function(a, b) {
+            var obj = translate(a, b),
+                value = obj.a + obj.b;
+
+            return obj.max ? value / Math.pow(10, obj.max) : value;
+        },
+
+        minus: function(a, b) {
+            return this.add(a, -b);
+        },
+
+        multiply: function(a, b) {
+            var obj = translate(a, b),
+                value = obj.a * obj.b;
+
+            return obj.max ? value / Math.pow(10, obj.max * 2) : value;
+        },
+
+        divide: function(a, b) {
+            var obj = translate(a, b);
+            return obj.a / obj.b;
+        }
+    }
+
+})();
