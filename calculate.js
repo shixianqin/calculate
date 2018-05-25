ad53daa677c9fd66b8a888573d00ae0f419877ed
shixianqin <1658852581@qq.com>
2018-05-25 12:33:11 +0800

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -3,46 +3,40 @@
  *   https://github.com/shixianqin/calculate
  */
 
-var Calculate = (function() {
+var Calculate = {
 
-    function translate(a, b) {
-        var dot = /\./,
-            empty = "",
-            dotA = a.toString().split(dot)[1] || empty,
-            dotB = b.toString().split(dot)[1] || empty,
-            max = Math.max(dotA.length, dotB.length);
+    _dotSize: function(num) {
+        return (num.toString().split('.')[1] || []).length;
+    },
 
+    _convert: function(a, b) {
+        var times = Math.pow(10, Math.max(this._dotSize(a), this._dotSize(b)));
         return {
-            a: Number(a.toFixed(max).replace(dot, empty)),
-            b: Number(b.toFixed(max).replace(dot, empty)),
-            max: max
+            a: a * times,
+            b: b * times,
+            times: times
         }
+    },
+
+    add: function(a, b) {
+        var obj = this._convert(a, b),
+            value = obj.a + obj.b;
+        return obj.times > 1 ? value / obj.times : value;
+    },
+
+    sub: function(a, b) {
+        return this.add(a, -b);
+    },
+
+    mul: function(a, b) {
+        var obj = this._convert(a, b),
+            value = obj.a * obj.b,
+            times = obj.times;
+        return times > 1 ? value / (times * times) : value;
+    },
+
+    div: function(a, b) {
+        var obj = this._convert(a, b);
+        return obj.a / obj.b;
     }
-
-    return {
-
-        add: function(a, b) {
-            var obj = translate(a, b),
-                value = obj.a + obj.b;
-
-            return obj.max ? value / Math.pow(10, obj.max) : value;
-        },
-
-        sub: function(a, b) {
-            return this.add(a, -b);
-        },
-
-        mul: function(a, b) {
-            var obj = translate(a, b),
-                value = obj.a * obj.b;
-
-            return obj.max ? value / Math.pow(10, obj.max * 2) : value;
-        },
-
-        div: function(a, b) {
-            var obj = translate(a, b);
-            return obj.a / obj.b;
-        }
-    }
-
-})();
+};
